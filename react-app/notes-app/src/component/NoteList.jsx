7af120c32137f7437dea2d18c5a1e514029e6228
HaRry2037 <harry10520@gmail.com>
2026-02-05 02:04:17 +0530

    not too many days

--- react-app/notes-app/src/component/NoteList.jsx
+++ react-app/notes-app/src/component/NoteList.jsx
@@ -1,4 +1,5 @@
-const NoteList = ({notes}) => {
+import Note from "./Note";
+const NoteList = ({notes, deleteNote}) => {
    if(notes.length ===0){
     <p className="text-center text-gray-500">
     No notes yet
@@ -8,19 +9,7 @@ const NoteList = ({notes}) => {
     return (  
 <div className="space-y-4">
     {notes.map((note)=>(
-        <div key={note.id} className="p-4 bg-white rounded-lg shadow-md border-l-4">
-<h3 className="text-lg font-bold">
-    {note.title}
-    <p className="text-sm text-gray-600">
-        <strong>Category : </strong>{note.category}
-    </p>
-    <p className="text-sm text-gray-600">
-        <strong>Priority : </strong>{note.priority}
-    </p>
-    <p className="mt-2">{note.description}</p>
-</h3>
-
-        </div>
+       <Note key={note.id} note={note} deleteNote={deleteNote}/>
 
     ))}
 </div>
